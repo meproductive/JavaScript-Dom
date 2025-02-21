005797d98f2f37e5dca83787aa1a81daf028ca10
meproductive <me.productive0@gmail.com>
2025-02-21 14:06:49 +0300

    menu items created in order through dom

--- dom-script.js
+++ dom-script.js
@@ -17,19 +17,21 @@ const ul = document.createElement('ul');
 ul.id = 'navMenu';
 ul.className = 'd-flex';
 document.getElementById('navigation').appendChild(ul);
+
 // for loop for li items and a items
-for (let i = 0; i < 4; i++){
+const MenuItems = ["HOME", "ABOUT ME", "BLOG", "CONTACT"];
+for (let i = 0; i < MenuItems.length; i++) {
         // li tag
         const li = document.createElement('li');
         li.id = 'li-item';
         ul.appendChild(li);
+    
         // a tag
         const a = document.createElement('a');
         a.id = 'a-item';
-        li.innerText = 'HOME';
+        a.innerText = MenuItems[i]; // Assign the current menu item to the a tag
         li.appendChild(a);
-
-}
+    }
 
 
 console.log('hello fucking world!');
